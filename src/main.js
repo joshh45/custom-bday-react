a17d2613c53b37a7a8638123276b4b321cb25ec3
Josh Ho <hojo1025@gmail.com>
2017-03-27 18:55:36 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './main.scss';
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+ReactDOM.render(
+  <h1> booyah </h1>,
+  document.getElementById('app')
+);
